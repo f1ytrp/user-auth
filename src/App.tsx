287a93e4781f105824b170e623c44369e5060ad1
flytrap <f20240755@pilani.bits-pilani.ac.in>
2025-06-14 18:45:02 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <form>
+        <input
+          placeholder='email'/>
+        <input/>
+        <input/>
+      </form>
+    </div>
+  )
+}
+
+export default App
